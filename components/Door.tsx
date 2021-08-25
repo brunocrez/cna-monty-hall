61f8426f188b3f4aea2c39fe83b8ba017529e8bc
Bruno Rezende <brunoc.rez@gmail.com>
2021-08-25 18:08:06 -0300

    logic to open the door

--- components/Door.tsx
+++ components/Door.tsx
@@ -9,19 +9,26 @@ interface DoorProps {
 
 export default function Door(props: DoorProps) {
   const door = props.value;
-  const selected = door.isSelected ? styles.selected : '';
+  const selected = door.isSelected && !door.isOpen ? styles.selected : '';
 
   const toggle = e => {
     props.onChange(door.toggleSelection());
   }
 
+  const open = e => {
+    e.stopPropagation();
+    props.onChange(door.open());
+  }
+
   return (
     <div className={styles.area} onClick={toggle}>
       <div className={`${styles.frame} ${selected}`}>
-        <div className={styles.door}>
-          <div className={styles.number}>{door.number}</div>
-          <div className={styles.doorKnob}></div>
-        </div>
+        { door.isOpen ? false : (
+          <div className={styles.door}>
+            <div className={styles.number}>{door.number}</div>
+            <div className={styles.doorKnob} onClick={open}></div>
+          </div>
+        )}
       </div>
       <div className={styles.ground}></div>
     </div>
